f91121d82fc9a4ef8463f9c66597b5c49cf2263f
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-04-01 11:54:10 -0400

    break point for this morning

---
+++ lib/artist.rb
@@ -0,0 +1,70 @@
+require 'pry'
+
+class Artist
+
+  extend Concerns::Findable
+
+  attr_accessor :name, :songs, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    if song.artist != self
+    song.artist = self
+    end
+    if !(@songs.include?(song))
+    @songs << song
+    end
+  end
+
+  def songs
+    @songs
+  end
+
+  # binding.pry
+
+  # def genres
+  #   self.songs.map do |song|
+  #     if song.genre != self
+  #       song.genre = self
+  #     end
+  #   end
+  #   self.songs.genre.uniq
+  # end
+  def genres
+    self.songs.collect {|song| song.genre}.uniq
+  end
+
+   def self.find_or_create_by_name(name)
+    @@all.each do |artist|
+      if artist.name == name
+        return artist
+      end
+    end
+    new(name).save
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,75 @@
+class Song
+
+
+
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    if artist != nil
+      self.artist = artist
+    end
+    if genre != nil
+      self.genre = genre
+    end
+  end
+
+  def artist=(artist)
+      @artist = artist
+      artist.add_song(self)
+  end
+
+  def genre=(genre)
+      @genre = genre
+      genre.add_song(self)
+  end
+
+  def self.all
+      @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ |song| song.name == name }
+  end
+
+   def self.find_or_create_by_name(name)
+    find_by_name(name)|| create(name)
+  end
+
+  # def self.new_from_filename(filename)
+  #   parts = filename.split(" - ")
+  #   artist_name = parts[0]
+  #   song_name = parts[1].gsub(".mp3", "")
+
+  #   song = self.new
+  #   song.name = song_name
+  #   song.artist_name = artist_name
+  #   song
+  # end
+
+  # def self.create_from_filename(filename)
+  #   parts = filename.split(" - ")
+  #   artist_name = parts[0]
+  #   song_name = parts[1].gsub(".mp3", "")
+
+  #   song = self.create
+  #   song.name = song_name
+  #   song.artist_name = artist_name
+  #   song
+  # end
+
+end
